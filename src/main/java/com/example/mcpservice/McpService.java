7dadecd0e226c251a462e219d6ae662a5cea3e04
dnsdl126 <dnsdl1217@naver.com>
2022-12-19 14:52:07 +0900

    commit

--- src/main/java/com/example/mcpservice/McpService.java
+++ src/main/java/com/example/mcpservice/McpService.java
@@ -1,10 +1,17 @@
 package com.example.mcpservice;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +19,15 @@ public class McpService {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @GetMapping("/test")
     public String greeting() {
         return "a";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST, consumes = "application/xml")
+    @ResponseBody
+    public String Xml(@RequestBody String body) {
+
+        return body;
     }
 }
