8418692c9c6e81043531ef4fe7fcb2e2b75df81c
JO Gyeongmin <gorogara87@gmail.com>
2022-12-19 21:15:30 +0900

    delete test

--- src/main/java/com/example/mcpservice/McpService.java
+++ src/main/java/com/example/mcpservice/McpService.java
@@ -1,10 +1,11 @@
 package com.example.mcpservice;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,21 +14,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class McpService {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @GetMapping("/test")
-    public String greeting() {
-        return "a";
-    }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = "application/xml")
     @ResponseBody
     public String Xml(@RequestBody String body) {
 
-        return body;
+        String url = "http://mms.smartnav.org:8088";
+        String clientMRN = "urn:mrn:mcp:device:smart:kor:bluemap-test-client";
+        String serviceMRN = "urn:mrn:mcp:service:smart:kor:instance:bluemap-mcp-service-sample";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "xml", Charset.forName("UTF-8")));
+        headers.set("dstMrn", clientMRN);
+        headers.set("srcMrn", serviceMRN);
+
+        String result = body;
+        HttpEntity<String> request = new HttpEntity<>(result, headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+
+        return "OK";
     }
 }
